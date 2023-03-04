a0f4868809b76621f423283e855a3e1a2685bb9a
Benson Domingo <benson.domingo@outlook.com>
2023-03-04 22:39:33 +0800

    update lumber models

--- alphacraft/lumber/models.py
+++ alphacraft/lumber/models.py
@@ -1,8 +1,10 @@
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django_extensions.db.models import TimeStampedModel
 
 
-class Source(TimeStampedModel):
+class Supplier(TimeStampedModel):
     name = models.CharField(max_length=100)
     address = models.TextField(null=True)
 
@@ -10,31 +12,60 @@ class Source(TimeStampedModel):
         return self.name
 
 
+class PurchaseOrder(TimeStampedModel):
+    source = models.ForeignKey(Supplier, on_delete=models.SET_NULL, null=True)
+    date = models.DateField()
+
+    paid = models.BooleanField(default=False)
+    date_paid = models.DateField(null=True)
+
+    balance = models.FloatField(null=True, blank=True)
+
+
+class PurchaseItem(TimeStampedModel):
+    purchase_order = models.ForeignKey(
+        PurchaseOrder,
+        on_delete=models.CASCADE,
+        related_name="items",
+    )
+    price = models.FloatField()
+    item_type = models.CharField(max_length=10)  # log/lumber
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveBigIntegerField()
+    item = GenericForeignKey("content_type", "object_id")
+
+
 class Log(TimeStampedModel):
-    thickness = models.FloatField()
-    width = models.FloatField()
-    length = models.FloatField()
-    source = models.ForeignKey(Source, on_delete=models.SET_NULL, null=True)
+    id = models.PositiveBigIntegerField(primary_key=True)
+    thickness = models.PositiveSmallIntegerField()
+    width = models.PositiveSmallIntegerField()
+    length = models.PositiveSmallIntegerField()
+    source = models.ForeignKey(Supplier, on_delete=models.SET_NULL, null=True)
+
+    purchase = GenericRelation(PurchaseItem)
 
     @property
     def volume(self):
         return (self.thickness * self.width * self.width) / 12
 
     def __str__(self) -> str:
-        return f"<{self.thickness}x{self.width}x{self.length} - {self.volume}"
+        return f"<Log {self.id}: {self.thickness} x {self.width} x {self.length} : {int(self.volume)} bdft>"
 
 
 class Lumber(TimeStampedModel):
-    thickness = models.FloatField()
-    width = models.FloatField()
-    length = models.FloatField()
-    log_source = models.ForeignKey(Log, on_delete=models.SET_NULL, null=True)
+    id = models.PositiveBigIntegerField(primary_key=True)
+    thickness = models.PositiveSmallIntegerField()
+    width = models.PositiveSmallIntegerField()
+    length = models.PositiveSmallIntegerField()
+    log = models.ForeignKey(Log, on_delete=models.SET_NULL, null=True)
+
     # bought directly from source
-    source = models.ForeignKey(Source, on_delete=models.SET_NULL, null=True)
+    purchase = GenericRelation(PurchaseItem)
 
     @property
     def volume(self):
         return (self.thickness * self.width * self.width) / 12
 
     def __str__(self) -> str:
-        return f"<{self.thickness}x{self.width}x{self.length} - {self.volume}"
+        return f"<Lumber: {self.thickness} x {self.width} x {self.length} : {int(self.volume)} bdft>"
